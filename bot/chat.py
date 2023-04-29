80b5b3f4f5c2881e3849b953dce9880a88213176
TeodorDevios <stepantimoshilov@yandex.ru>
2023-04-29 13:01:30 +0300

    Функция выбора мероприятий, кнопки в start

--- bot/chat.py
+++ bot/chat.py
@@ -1,16 +1,27 @@
 from aiogram.types import Message
 from aiogram import Dispatcher
 from config import texts, bot, dp
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 
 async def start_message(message: Message):
-    await bot.send_message(message.chat.id, texts['start'], parse_mode='HTML')
+    key = ReplyKeyboardMarkup(resize_keyboard=True)
+    key.add(KeyboardButton('Помощь'), KeyboardButton('Мероприятия'))
+    await bot.send_message(message.chat.id, texts['start'], parse_mode='HTML', reply_markup=key)
 
 
 async def help_message(message: Message):
     await bot.send_message(message.chat.id, f'Лечу на помощь: \n{texts["help"]}')
 
 
+async def change_day(message: Message):
+    key = ReplyKeyboardMarkup(resize_keyboard=True)
+    key.add(KeyboardButton('Сегодня'), KeyboardButton('Завтра'), KeyboardButton('На этой неделе'),
+            KeyboardButton('В это месяце'))
+    await bot.send_message(message.chat.id, 'Выбери дату мероприятия:', reply_markup=key)
+
+
 def init_chat(dp: Dispatcher):
     dp.register_message_handler(start_message, commands=['start'])
-    dp.register_message_handler(help_message, commands=['help'])
+    dp.register_message_handler(help_message, lambda message: message.text == "Помощь")
+    dp.register_message_handler(change_day, lambda message: message.text == "Мероприятия")
